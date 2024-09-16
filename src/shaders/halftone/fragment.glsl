4afe306851b478536005544849a99789e5e11ac7
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2024-09-16 12:04:54 -0400

    added halftone shadow and light colors

--- src/shaders/halftone/fragment.glsl
+++ src/shaders/halftone/fragment.glsl
@@ -1,11 +1,40 @@
 uniform vec3 uColor;
 uniform vec2 uResolution;
+uniform float uShadowRepetitions;
+uniform vec3 uShadowColor;
+uniform float uLightRepetitions;
+uniform vec3 uLightColor;
 
 varying vec3 vNormal;
 varying vec3 vPosition;
 
 #include ../includes/ambientLight.glsl
 #include ../includes/directionalLight.glsl
+
+vec3 halftone(
+    vec3 color,
+    float repetitions,
+    vec3 direction,
+    float low,
+    float high,
+    vec3 pointColor,
+    vec3 normal
+)
+{
+    float intensity = dot(normal, direction);
+    intensity = smoothstep(low, high, intensity);
+
+    vec2 uv = gl_FragCoord.xy / uResolution.y;
+    uv *= repetitions;
+    uv = mod(uv, 1.0);
+
+    float point = distance(uv, vec2(0.5));
+    point = 1.0 - step(0.5 * intensity, point);
+
+    color = mix(color, pointColor, point);
+
+    return mix(color, pointColor, point);
+}
 
 void main()
 {
@@ -32,23 +61,28 @@ void main()
     color *= light;
 
     // Halftone
-    float repitions = 50.0;
-    vec3 direction = vec3(0.0, -1.0, 0.0);
-    float low = -0.8;
-    float high = 1.5;
+    color = halftone(
+        color,                 // Input color
+        uShadowRepetitions,                  // Repetitions
+        vec3(0.0, - 1.0, 0.0), // Direction
+        - 0.8,                 // Low
+        1.5,                   // High
+        uShadowColor,   // Point color
+        normal                 // Normal
+    );
 
-    float intensity = dot(normal, direction);
-    intensity = smoothstep(low, high, intensity);
-
-    vec2 uv = gl_FragCoord.xy / uResolution.y;
-    uv *= repitions;
-    uv = mod(uv, 1.0);
-
-    float point = distance(uv, vec2(0.5));
-    point = 1.0 - step(0.5 * intensity, point);
+    color = halftone(
+        color,               // Input color
+        uLightRepetitions,   // Repetitions
+        vec3(1.0, 1.0, 0.0), // Direction
+        0.5,                 // Low
+        1.5,                 // High
+        uLightColor,         // Point color
+        normal               // Normal
+    );
  
     // Final color
-    gl_FragColor = vec4(point, point, point, 1.0);
+    gl_FragColor = vec4(color, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
